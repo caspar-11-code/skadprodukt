64255933b39e10609c699fe8f8a131bc9ed19d2b
PerKacper <hkacper111@gmail.com>
2026-07-06 12:12:29 +0200

    marki: +Heinz (Kraft Heinz US), Żywiec Zdrój (Danone FR), Vegeta (Podravka HR), Lajkonik (Bahlsen DE); +kraj Chorwacja; tło shortów pełniejsze

--- build.js
+++ build.js
@@ -31,7 +31,7 @@ const COUNTRIES = {
   ES: { pl: 'Hiszpania', en: 'Spain' }, NO: { pl: 'Norwegia', en: 'Norway' }, CZ: { pl: 'Czechy', en: 'Czechia' },
   EG: { pl: 'Egipt', en: 'Egypt' }, CO: { pl: 'Kolumbia', en: 'Colombia' }, PE: { pl: 'Peru', en: 'Peru' },
   EC: { pl: 'Ekwador', en: 'Ecuador' }, SK: { pl: 'Słowacja', en: 'Slovakia' }, KW: { pl: 'Kuwejt', en: 'Kuwait' },
-  HK: { pl: 'Hongkong', en: 'Hong Kong' }, XX: { pl: 'różne kraje', en: 'various countries' },
+  HK: { pl: 'Hongkong', en: 'Hong Kong' }, HR: { pl: 'Chorwacja', en: 'Croatia' }, XX: { pl: 'różne kraje', en: 'various countries' },
   EU: { pl: 'Unia Europejska (różne)', en: 'European Union (various)' },
 };
 
